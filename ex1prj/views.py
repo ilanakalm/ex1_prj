f51796978adc24bb8e148318ae26a09a8ef98720
ilanakalm <ilana.kalm@gmail.com>
2022-08-24 14:39:25 +0300

    Add reverse

--- ex1prj/views.py
+++ ex1prj/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 def home(request):
     return render(request,"home.html")
+
+def reverse(request):
+    get_text=request.GET['message']
+    reverse_text=get_text[::-1]
+
+
+
+    return render(request,"reverse.html",{'reversetext':reverse_text,'gettext':get_text})
